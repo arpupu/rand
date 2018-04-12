a81a681d9200dcefe04bfead41113b0946be60cc
m305-60\w401 <sam43101@yahoo.com.tw>
2018-04-12 17:36:24 +0800

    作業ok

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            // 1. 將所有號碼都放入List之中
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 42; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // 2. 隨機挑選其中一個印出
+            Random rand = new Random();
+            for (int i =0; i <6; i++)
+            {
+                int r = rand.Next(1, numbers.Count);
+                Console.WriteLine(numbers[r]);
+
+                // 3. 並從List之中刪除
+                numbers.RemoveAt(r);
+            }
+            Console.ReadKey();
         }
+        
     }
 }
+
